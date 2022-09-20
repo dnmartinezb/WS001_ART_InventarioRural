17ad0ab50c38fa3d67ebb43cc2121aa74e888681
Diego Nicolas Martinez Becerra <DMartinez@minvivienda.gov.co>
2022-09-20 15:04:59 -0500

    ajuste retirar coma del appsettings y parametros nullable

--- WS001_ART_InventarioRural/Models/InventarioRural.cs
+++ WS001_ART_InventarioRural/Models/InventarioRural.cs
@@ -7,47 +7,47 @@ namespace WS001_ART_InventarioRural.Models
         [Key]
         public int Id { get; set; }
 
-        public string CodVereda { get; set; }
+        public string? CodVereda { get; set; }
 
-        public string NomDepartamento { get; set; }
+        public string? NomDepartamento { get; set; }
 
-        public string NomMunicipio { get; set; }
+        public string? NomMunicipio { get; set; }
 
-        public string NomVereda { get; set; }
+        public string? NomVereda { get; set; }
 
-        public string TipoComunidadEncuestada { get; set; }
+        public string? TipoComunidadEncuestada { get; set; }
 
-        public string NomComunidadEncuestada { get; set; }
+        public string? NomComunidadEncuestada { get; set; }
 
-        public int NumViviendas { get; set; }
+        public int? NumViviendas { get; set; }
 
-        public int NumHabitantes { get; set; }
+        public int? NumHabitantes { get; set; }
 
-        public string TipoTomaAgua { get; set; }
+        public string? TipoTomaAgua { get; set; }
 
-        public string TipoInfraestructuraAbAgua { get; set; }
+        public string? TipoInfraestructuraAbAgua { get; set; }
 
-        public string NomRespAbastecimientoAgua { get; set; }
+        public string? NomRespAbastecimientoAgua { get; set; }
 
-        public string TelefonoResponsable { get; set; }
+        public string? TelefonoResponsable { get; set; }
 
-        public string CorreoElectronicoResponsable { get; set; }
+        public string? CorreoElectronicoResponsable { get; set; }
 
-        public string TipoPoblacionAbaAgua { get; set; }
+        public string? TipoPoblacionAbaAgua { get; set; }
 
-        public string TipoInfraestructuraAguasRes { get; set; }
+        public string? TipoInfraestructuraAguasRes { get; set; }
 
-        public string NomResponsableAguasResiduales { get; set; }
+        public string? NomResponsableAguasResiduales { get; set; }
 
-        public string TelefonoResponsableAguasRes { get; set; }
+        public string? TelefonoResponsableAguasRes { get; set; }
 
-        public string CorreoElectronicoResAguasRes { get; set; }
+        public string? CorreoElectronicoResAguasRes { get; set; }
 
-        public string TipoManejoResiduosSolidos { get; set; }
+        public string? TipoManejoResiduosSolidos { get; set; }
 
-        public string NomCentroEducativoComunidad { get; set; }
+        public string? NomCentroEducativoComunidad { get; set; }
 
-        public string NomCentroSaludComunidad { get; set; }
+        public string? NomCentroSaludComunidad { get; set; }
 
     }
 }
